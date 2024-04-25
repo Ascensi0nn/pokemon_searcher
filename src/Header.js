04fcb4d1653e0d33ce8a34ff6e9827837ecd818e
Owen Luts <owenlutsdev@gmail.com>
2024-04-25 10:15:33 -0400

    skeleton

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <div class="flex flex-row justify-evenly">
+      <input class="border-none"></input>
+    </div>
+  );
+}
+
+export default Header;
